177ad74ec89a608a6a497245daec03230a47c082
Asad Ali <asadalibest1@gmail.com>
2026-05-24 02:32:27 +0500

    blogs and review api for home page

--- components/common/Blogs.jsx
+++ components/common/Blogs.jsx
@@ -1,23 +1,48 @@
 "use client";
-import { blogSlides } from "@/data/blogs";
+// import { blogSlides } from "@/data/blogs";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 export default function Blogs({ parentClass = "section-blog tf-section" }) {
+  const [BlogsListing, setBlogsListing] = useState([]);
+  const [BlogsLoading, setBlogsLoading] = useState(true);
+
+  const fecthBlogs = async () => {
+    setBlogsLoading(true);
+    const getBlogsRequest = await fetch(
+      "https://carsymotors.com.au/api_carsy/carsy_api/cars_api.php?action=get_whats_new",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer dfsdgs43543534543gdsfdsfdsfa22342222@@222",
+        },
+      },
+    );
+    const getBlogsResponse = await getBlogsRequest.json();
+    setBlogsListing(getBlogsResponse?.data);
+    setBlogsLoading(false);
+  };
+  useEffect(() => {
+    fecthBlogs();
+  }, []);
+
   return (
-    <section className={parentClass}>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className=" flex align-center justify-space flex-wrap gap-20">
-              <h2
-                className="wow fadeInUpSmall"
-                data-wow-delay="0.2s"
-                data-wow-duration="1000ms"
-              >
-                What's New?
-              </h2>
-              {/* <Link
+    <>
+      <section className={parentClass}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className=" flex align-center justify-space flex-wrap gap-20">
+                <h2
+                  className="wow fadeInUpSmall"
+                  data-wow-delay="0.2s"
+                  data-wow-duration="1000ms"
+                >
+                  What's New?
+                </h2>
+                {/* <Link
                 href={`/blog-grid`}
                 className="tf-btn-arrow wow fadeInUpSmall"
                 data-wow-delay="0.2s"
@@ -26,65 +51,78 @@ export default function Blogs({ parentClass = "section-blog tf-section" }) {
                 View all
                 <i className="icon-autodeal-btn-right" />
               </Link> */}
-            </div>
-            <p className="mt-18 heading-section">
-              Find all the latest and intresting news about our cars here!
-            </p>
-            <Swiper
-              className="swiper tf-sw-mobile"
-              slidesPerView={3}
-              breakpoints={{
-                1024: {
-                  slidesPerView: 3,
-                },
-                768: {
-                  slidesPerView: 2,
-                },
-                0: {
-                  slidesPerView: 1,
-                },
-              }}
-              spaceBetween={30}
-            >
-              {blogSlides.map((slide, index) => (
-                <SwiperSlide className="swiper-slide" key={index}>
-                  <div className="blog-article-item style1 hover-img">
-                    <div className="images img-style relative flex-none">
-                      <Image
-                        className="lazyload"
-                        data-src={slide.imgSrc}
-                        alt={slide.imgAlt}
-                        src={slide.imgSrc}
-                        width={slide.imgWidth}
-                        height={slide.imgHeight}
-                      />
-                      <div className="date">{slide.date}</div>
-                    </div>
-                    <div className="content">
-                      <div className="sub-box flex align-center fs-13 fw-6">
-                        <a href="javascript:void(0)" className="admin fw-7 text-color-2">
-                          {slide.admin}
-                        </a>
-                        <a href="javascript:void(0)" className="category text-color-3">
-                          {slide.category}
-                        </a>
-                      </div>
-                      <h3>
-                        <Link href={`/blog-detail/${slide.id}`}>
-                          {slide.title}
-                        </Link>
-                      </h3>
-                      <p>{slide.description}</p>
-                    </div>
+              </div>
+              <p className="mt-18 heading-section">
+                Find all the latest and intresting news about our cars here!
+              </p>
+              <Swiper
+                className="swiper tf-sw-mobile"
+                slidesPerView={3}
+                breakpoints={{
+                  1024: {
+                    slidesPerView: 3,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                  },
+                  0: {
+                    slidesPerView: 1,
+                  },
+                }}
+                spaceBetween={30}
+              >
+                {BlogsLoading ? (
+                  <div className="center my-5">
+                    <span className="loader"></span>
                   </div>
-                </SwiperSlide>
-              ))}
+                ) : (
+                  BlogsListing.map((slide, index) => (
+                    <SwiperSlide className="swiper-slide" key={index}>
+                      <div className="blog-article-item style1 hover-img">
+                        <div className="images img-style relative flex-none">
+                          <Image
+                            className="lazyload"
+                            data-src={slide.image}
+                            alt={slide.link_name}
+                            src={slide.image}
+                            width={500}
+                            height={300}
+                          />
+                          <div className="date">{slide.created_at.split(" ")[0]}</div>
+                        </div>
+                        <div className="content">
+                          <div className="sub-box flex align-center fs-13 fw-6">
+                            <a
+                              href="javascript:void(0)"
+                              className="admin fw-7 text-color-2"
+                            >
+                              {slide.title}
+                            </a>
+                            <a
+                              href="javascript:void(0)"
+                              className="category text-color-3"
+                            >
+                              {slide?.category}
+                            </a>
+                          </div>
+                          <h3>
+                            <Link href={`/blog-detail/${slide.id}`}>
+                              {slide.link_name}
+                            </Link>
+                          </h3>
+                          <p>{slide.short_description}</p>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  ))
+                )}
 
-              <div className="swiper-pagination3" />
-            </Swiper>
+                <div className="swiper-pagination3" />
+              </Swiper>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
--- components/homes/home-7/Testimonials.jsx
+++ components/homes/home-7/Testimonials.jsx
@@ -4,7 +4,31 @@ import { testimonials } from "@/data/testimonials";
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 export default function Testimonials() {
+  const [ReviewsListing, setReviewsListing] = useState([]);
+  const [ReviewsLoading, setReviewsLoading] = useState(true);
+
+  const fecthReviews = async () => {
+    setReviewsLoading(true);
+    const getReviewsRequest = await fetch(
+      "https://carsymotors.com.au/api_carsy/carsy_api/cars_api.php?action=get_reviews",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer dfsdgs43543534543gdsfdsfdsfa22342222@@222",
+        },
+      },
+    );
+    const getReviewsResponse = await getReviewsRequest.json();
+    setReviewsListing(getReviewsResponse?.data);
+    setReviewsLoading(false);
+  };
+  useEffect(() => {
+    fecthReviews();
+  }, []);
+
   const swiperOptions = {
     // autoplay: {
     //     delay: 5000,
@@ -52,52 +76,60 @@ export default function Testimonials() {
             </div>
           </div>
           <div className="col-lg-12">
-            <Swiper
-              {...swiperOptions}
-              modules={[Navigation, Pagination]}
-              className="swiper-container carousel-6 mask-0-3"
-            >
-              {testimonials.slice(0, 6).map((elm, i) => (
-                <SwiperSlide key={i} className="swiper-slide">
-                  <div className="tf-testimonial style-3 box-tes center">
-                    <div className="inner-top">
-                      <div className="icon">
-                        <svg
-                          width={36}
-                          height={36}
-                          viewBox="0 0 36 36"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M9.74992 4.5C14.5409 4.5 17.9999 8.4555 17.9999 14.313C17.9699 22.8225 11.5724 28.839 2.34292 29.994C1.48792 30.102 1.15792 28.914 1.94542 28.5645C5.48542 26.9895 7.27492 24.9915 7.50592 23.0145C7.67842 21.537 6.87592 20.2425 5.86642 19.9995C3.25192 19.371 1.49992 16.1145 1.49992 12.75C1.49992 10.562 2.36911 8.46354 3.91628 6.91637C5.46346 5.36919 7.56188 4.5 9.74992 4.5ZM27.7499 4.5C32.5409 4.5 35.9999 8.4555 35.9999 14.313C35.9699 22.8225 29.5724 28.839 20.3429 29.994C19.4879 30.102 19.1579 28.914 19.9454 28.5645C23.4854 26.9895 25.2749 24.9915 25.5059 23.0145C25.6784 21.537 24.8759 20.2425 23.8664 19.9995C21.2519 19.371 19.4999 16.1145 19.4999 12.75C19.4999 10.562 20.3691 8.46354 21.9163 6.91637C23.4635 5.36919 25.5619 4.5 27.7499 4.5Z"
-                            fill="CurrentColor"
+            {ReviewsLoading ? (
+              <div className="center my-5">
+                <span className="loader"></span>
+              </div>
+            ) : (
+              <Swiper
+                {...swiperOptions}
+                modules={[Navigation, Pagination]}
+                className="swiper-container carousel-6 mask-0-3"
+              >
+                {ReviewsListing.map((elm, i) => (
+                  <SwiperSlide key={i} className="swiper-slide">
+                    <div className="tf-testimonial style-3 box-tes center">
+                      <div className="inner-top">
+                        <div className="icon">
+                          <svg
+                            width={36}
+                            height={36}
+                            viewBox="0 0 36 36"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M9.74992 4.5C14.5409 4.5 17.9999 8.4555 17.9999 14.313C17.9699 22.8225 11.5724 28.839 2.34292 29.994C1.48792 30.102 1.15792 28.914 1.94542 28.5645C5.48542 26.9895 7.27492 24.9915 7.50592 23.0145C7.67842 21.537 6.87592 20.2425 5.86642 19.9995C3.25192 19.371 1.49992 16.1145 1.49992 12.75C1.49992 10.562 2.36911 8.46354 3.91628 6.91637C5.46346 5.36919 7.56188 4.5 9.74992 4.5ZM27.7499 4.5C32.5409 4.5 35.9999 8.4555 35.9999 14.313C35.9699 22.8225 29.5724 28.839 20.3429 29.994C19.4879 30.102 19.1579 28.914 19.9454 28.5645C23.4854 26.9895 25.2749 24.9915 25.5059 23.0145C25.6784 21.537 24.8759 20.2425 23.8664 19.9995C21.2519 19.371 19.4999 16.1145 19.4999 12.75C19.4999 10.562 20.3691 8.46354 21.9163 6.91637C23.4635 5.36919 25.5619 4.5 27.7499 4.5Z"
+                              fill="CurrentColor"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                      <p className="fs-16 lh-22 text-color-2">
+                        {elm.description}
+                      </p>
+                      <div className="author-box flex">
+                        <div className="images">
+                          <Image
+                            className="ls-is-cached lazyloaded"
+                            alt="images"
+                            src={elm.image}
+                            width={57}
+                            height={57}
                           />
-                        </svg>
+                        </div>
+                        <div className="content">
+                          <h5>{elm.name}</h5>
+                          <p className="fs-12 lh-16">{elm.designation}</p>
+                        </div>
                       </div>
                     </div>
-                    <p className="fs-16 lh-22 text-color-2">"{elm.text}"</p>
-                    <div className="author-box flex">
-                      <div className="images">
-                        <Image
-                          className="ls-is-cached lazyloaded"
-                          alt="images"
-                          src={elm.authorImage}
-                          width={57}
-                          height={57}
-                        />
-                      </div>
-                      <div className="content">
-                        <h5>{elm.authorName}</h5>
-                        <p className="fs-12 lh-16">{elm.authorTitle}</p>
-                      </div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              ))}
+                  </SwiperSlide>
+                ))}
 
-              <div className="swiper-pagination3 spd18 pb-1" />
-            </Swiper>
+                <div className="swiper-pagination3 spd18 pb-1" />
+              </Swiper>
+            )}
           </div>
         </div>
       </div>

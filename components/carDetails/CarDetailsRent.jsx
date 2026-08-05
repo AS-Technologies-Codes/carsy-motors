3b8a0a3b9f21dc0683cfa3cc07379cbe36ea91d2
Asad Ali <asadalibest1@gmail.com>
2026-08-06 00:03:43 +0500

    rent multi page ui updated

--- components/carDetails/CarDetailsRent.jsx
+++ components/carDetails/CarDetailsRent.jsx
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import CarInfo from './detailComponents/CarInfoRent';
 import RentBooking from './RentBooking';
 import { useCarFilter } from '@/context/providers/CarFilterContext';
+import toast from 'react-hot-toast';
 
 const CarDetailsRent = ({ carItem }) => {
   const [currentStep, setCurrentStep] = useState(2);
@@ -42,16 +43,56 @@ const CarDetailsRent = ({ carItem }) => {
   //   "total_amount": 1000,
   //   "payment_type": CarDetailsListing?.payment_type
   // }
-  
 
 
+  const changesRemoved = (t) => {
+    window.localStorage.removeItem("filters");
+    router.push(`/rentals/${CarDetailsListing.rent_type}`);
+    toast.dismiss(t.id);
+  }
   const STEPS = [
-    { key: 0, label: "Car Selected" },
-    { key: 1, label: "Car Review" },
-    { key: 2, label: "Protection & Extras" },
-    { key: 3, label: "Booking" },
+    {
+      key: 0,
+      icon: "",
+      label: "Car Selected"
+    },
+    {
+      key: 1,
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="inherit" height="inherit" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-car-front-icon lucide-car-front"><path d="m21 8-2 2-1.5-3.7A2 2 0 0 0 15.646 5H8.4a2 2 0 0 0-1.903 1.257L5 10 3 8" /><path d="M7 14h.01" /><path d="M17 14h.01" /><rect width="18" height="8" x="3" y="10" rx="2" /><path d="M5 18v2" /><path d="M19 18v2" /></svg>,
+      label: "Car Review"
+    },
+    {
+      key: 2,
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="inherit" height="inherit" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="second lucide lucide-shield-cog-corner-icon lucide-shield-cog-corner"><path d="M11 22c-3.806-1.45-7-3.966-7-9V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1v4" /><path d="M14.923 16.547 14 16.164" /><path d="m14.923 18.843-.923.383" /><path d="M16.547 14.923 16.164 14" /><path d="m16.547 20.467-.383.924" /><path d="m18.843 14.923.383-.923" /><path d="m19.225 21.391-.382-.924" /><path d="m20.467 16.547.923-.383" /><path d="m20.467 １８.843.923.383" /><circle cx="１７．６９５" cy="１７．６９５" r="３" /></svg>,
+      label: "Protection & Extras"
+    },
+    {
+      key: 3,
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="inherit" height="inherit" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="second lucide lucide-user-round-key-icon lucide-user-round-key"><path d="M19 11v6" /><path d="M19 13h2" /><path d="M2 21a8 8 0 0 1 12.868-6.349" /><circle cx="10" cy="8" r="5" /><circle cx="19" cy="19" r="2" /></svg>,
+      label: "Booking"
+    },
   ];
 
+  const showActionToast = () => {
+    toast((t) => (
+      <div className="gap-3 flex justify-content-center flex-column align-items-center">
+        {/* <h1 className="fs-4">Please select an age</h1> */}
+        <div className="fw-bold mt-3">Alert!</div>
+        <div className="center">Are you sure you want to de-select all changes?</div>
+
+        <button
+          onClick={() => changesRemoved(t)}
+          className="sc-button border-0"
+        >
+          <span>Yes</span>
+        </button>
+      </div>
+    ), {
+      // Optional: Stop the toast from automatically hiding 
+      // so the user is forced to click "OK"
+      duration: Infinity,
+    });
+  };
 
   if (!CarDetailsListing?.title) {
     return <CarReview
@@ -90,14 +131,17 @@ const CarDetailsRent = ({ carItem }) => {
         {STEPS.map((step, index) => (
           <div
             key={step.key}
-            onClick={() => setCurrentStep(currentStep)}
+            onClick={() => index == 0 ? showActionToast() : setCurrentStep(index + 1)}
             className={`msf-progress-item ${index === currentStep - 1 ? "is-active" : ""
               } ${index < (currentStep - 1) ? "is-complete" : ""}`}
           >
-            <span className="msf-progress-dot">
-              {index < (currentStep - 1) ? "✓" : index + 1}
-            </span>
-            <span className="msf-progress-label">{step.label}</span>
+            {step.icon ?
+              step.icon :
+              <span className="msf-progress-dot">
+                {index < (currentStep - 1) ? "✓" : index + 1}
+              </span>
+            }
+            <h2 className="msf-progress-label">{step.label}</h2>
           </div>
         ))}
       </div>

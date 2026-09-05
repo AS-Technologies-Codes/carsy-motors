678c858c4109ccbeb7b4688f9f5151a05c8d85c8
Asad Ali <asadalibest1@gmail.com>
2026-09-05 16:23:22 +0500

    ui issues pagination and slider

--- app/(other-pages)/favourites/page.jsx
+++ app/(other-pages)/favourites/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Slider1 from "@/components/carDetails/sliders/Slider1";
+import Slider1 from "@/components/carDetails/sliders/SliderViewer";
 import Footer1 from "@/components/footers/Footer1";
 import Header1 from "@/components/headers/Header1";
 import Hero from "@/components/homes/home-6/Hero";
@@ -81,42 +81,10 @@ const page = () => {
                         {" "}
                         {Favourites.map((car, i) => (
                           <div key={i} className="box-car-list style-2 hv-one">
-                            <div className="image-group relative">
-                              <div className="top flex-two">
-                                <ul className="d-flex gap-8">
-                                  <li className="flag-tag success">
-                                    {car.featured ? "Featured" : ""}
-                                  </li>
-                                  <li className="flag-tag style-1">
-                                    <div className="icon">
-                                      <svg
-                                        width={16}
-                                        height={13}
-                                        viewBox="0 0 16 13"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                      >
-                                        <path
-                                          d="M1.5 9L4.93933 5.56067C5.07862 5.42138 5.24398 5.31089 5.42597 5.2355C5.60796 5.16012 5.80302 5.12132 6 5.12132C6.19698 5.12132 6.39204 5.16012 6.57403 5.2355C6.75602 5.31089 6.92138 5.42138 7.06067 5.56067L10.5 9M9.5 8L10.4393 7.06067C10.5786 6.92138 10.744 6.81089 10.926 6.7355C11.108 6.66012 11.303 6.62132 11.5 6.62132C11.697 6.62132 11.892 6.66012 12.074 6.7355C12.256 6.81089 12.4214 6.92138 12.5607 7.06067L14.5 9M2.5 11.5H13.5C13.7652 11.5 14.0196 11.3946 14.2071 11.2071C14.3946 11.0196 14.5 10.7652 14.5 10.5V2.5C14.5 2.23478 14.3946 1.98043 14.2071 1.79289C14.0196 1.60536 13.7652 1.5 13.5 1.5H2.5C2.23478 1.5 1.98043 1.60536 1.79289 1.79289C1.60536 1.98043 1.5 2.23478 1.5 2.5V10.5C1.5 10.7652 1.60536 11.0196 1.79289 11.2071C1.98043 11.3946 2.23478 11.5 2.5 11.5ZM9.5 4H9.50533V4.00533H9.5V4ZM9.75 4C9.75 4.0663 9.72366 4.12989 9.67678 4.17678C9.62989 4.22366 9.5663 4.25 9.5 4.25C9.4337 4.25 9.37011 4.22366 9.32322 4.17678C9.27634 4.12989 9.25 4.0663 9.25 4C9.25 3.9337 9.27634 3.87011 9.32322 3.82322C9.37011 3.77634 9.4337 3.75 9.5 3.75C9.5663 3.75 9.62989 3.77634 9.67678 3.82322C9.72366 3.87011 9.75 3.9337 9.75 4Z"
-                                          stroke="white"
-                                          strokeWidth="1.5"
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                        />
-                                      </svg>
-                                    </div>
-                                    {car.images.length}
-                                  </li>
-                                </ul>
-                                <div className="year flag-tag">{car.year}</div>
-                              </div>
-
-                              <div className="img-style">
-                                <Slider1
-                                  images={car?.images.map((img) => img.src)}
-                                />
-                              </div>
-                            </div>
+                            <Slider1
+                              car={car}
+                              images={car?.images.map((img) => img.src)}
+                            />
                             <div className="content">
                               <div className="inner1">
                                 <div className="text-address">
---
+++ components/carDetails/sliders/SliderViewer.jsx
@@ -0,0 +1,149 @@
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import Image from "next/image";
+import Product360Viewer from "@/components/product360Viewer/Product360Viewer";
+import ImageViewer from "react-simple-image-viewer";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+export default function Slider1({ car, images, viewer }) {
+  const [Toggle, setToggle] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
+
+  return (
+    <div className="image-group relative">
+      <div className="top flex-two">
+        <ul className="d-flex gap-8">
+          <li className="flag-tag success">
+            {car.featured ? "Featured" : ""}
+          </li>
+          <li className="flag-tag style-1 cursor-pointer"
+                  onClick={() => openImageViewer(1)}>
+            <div className="icon">
+              <svg
+                width={16}
+                height={13}
+                viewBox="0 0 16 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.5 9L4.93933 5.56067C5.07862 5.42138 5.24398 5.31089 5.42597 5.2355C5.60796 5.16012 5.80302 5.12132 6 5.12132C6.19698 5.12132 6.39204 5.16012 6.57403 5.2355C6.75602 5.31089 6.92138 5.42138 7.06067 5.56067L10.5 9M9.5 8L10.4393 7.06067C10.5786 6.92138 10.744 6.81089 10.926 6.7355C11.108 6.66012 11.303 6.62132 11.5 6.62132C11.697 6.62132 11.892 6.66012 12.074 6.7355C12.256 6.81089 12.4214 6.92138 12.5607 7.06067L14.5 9M2.5 11.5H13.5C13.7652 11.5 14.0196 11.3946 14.2071 11.2071C14.3946 11.0196 14.5 10.7652 14.5 10.5V2.5C14.5 2.23478 14.3946 1.98043 14.2071 1.79289C14.0196 1.60536 13.7652 1.5 13.5 1.5H2.5C2.23478 1.5 1.98043 1.60536 1.79289 1.79289C1.60536 1.98043 1.5 2.23478 1.5 2.5V10.5C1.5 10.7652 1.60536 11.0196 1.79289 11.2071C1.98043 11.3946 2.23478 11.5 2.5 11.5ZM9.5 4H9.50533V4.00533H9.5V4ZM9.75 4C9.75 4.0663 9.72366 4.12989 9.67678 4.17678C9.62989 4.22366 9.5663 4.25 9.5 4.25C9.4337 4.25 9.37011 4.22366 9.32322 4.17678C9.27634 4.12989 9.25 4.0663 9.25 4C9.25 3.9337 9.27634 3.87011 9.32322 3.82322C9.37011 3.77634 9.4337 3.75 9.5 3.75C9.5663 3.75 9.62989 3.77634 9.67678 3.82322C9.72366 3.87011 9.75 3.9337 9.75 4Z"
+                  stroke="white"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            {car.images.length}
+          </li>
+        </ul>
+        <div className="year flag-tag">
+          {car.year}
+        </div>
+      </div>
+      <div className="img-style buy-car-slider">
+
+        <div className="relative">
+          {Toggle ? (
+            <div className="rounded-3 overflow-hidden">
+              <Product360Viewer images={images} height={550} />
+            </div>
+          ) : (
+            <Carousel className="rounded-3 overflow-hidden">
+              {
+                images?.map((elm, index) => <div className="cursor-pointer rounded-bottom-3 overflow-hidden">
+                  <Image src={elm} width={1280} height={853} />
+                </div>)
+              }
+            </Carousel>
+          )}
+
+
+          {isViewerOpen && (
+            <ImageViewer
+              src={images}
+              currentIndex={currentImage}
+              onClose={closeImageViewer}
+              disableScroll={true}
+              backgroundStyle={{
+                backgroundColor: "rgba(0,0,0,0.9)",
+                zIndex: 999
+              }}
+              closeOnClickOutside={true}
+            />
+          )}
+
+          {viewer?.length ? (
+            <div
+              className="d-flex"
+              style={{ position: "absolute", zIndex: 999, bottom: Toggle ? 40 : 80, left: 10 }}
+            >
+              <a className="specs-features mx-2" onClick={() => setToggle(true)}>
+                <div className="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-box-icon lucide-box"
+                  >
+                    <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
+                    <path d="m3.3 7 8.7 5 8.7-5" />
+                    <path d="M12 22V12" />
+                  </svg>
+                </div>
+                <span className="fw-5 w-100 font text-color-2 lh-16">View 360</span>
+              </a>
+              <a
+                className="specs-features image"
+                onClick={() => setToggle(false)}
+                data-pswp-width="1245"
+                data-pswp-height="701"
+              >
+                <div className="icon">
+                  <svg
+                    width={18}
+                    height={14}
+                    viewBox="0 0 18 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M0.875 10.125L5.17417 5.82583C5.34828 5.65172 5.55498 5.51361 5.78246 5.41938C6.00995 5.32515 6.25377 5.27665 6.5 5.27665C6.74623 5.27665 6.99005 5.32515 7.21754 5.41938C7.44502 5.51361 7.65172 5.65172 7.82583 5.82583L12.125 10.125M10.875 8.875L12.0492 7.70083C12.2233 7.52672 12.43 7.38861 12.6575 7.29438C12.885 7.20015 13.1288 7.15165 13.375 7.15165C13.6212 7.15165 13.865 7.20015 14.0925 7.29438C14.32 7.38861 14.5267 7.52672 14.7008 7.70083L17.125 10.125M2.125 13.25H15.875C16.2065 13.25 16.5245 13.1183 16.7589 12.8839C16.9933 12.6495 17.125 12.3315 17.125 12V2C17.125 1.66848 16.9933 1.35054 16.7589 1.11612C16.5245 0.881696 16.2065 0.75 15.875 0.75H2.125C1.79348 0.75 1.47554 0.881696 1.24112 1.11612C1.0067 1.35054 0.875 1.66848 0.875 2V12C0.875 12.3315 1.0067 12.6495 1.24112 12.8839C1.47554 13.1183 1.79348 13.25 2.125 13.25ZM10.875 3.875H10.8817V3.88167H10.875V3.875ZM11.1875 3.875C11.1875 3.95788 11.1546 4.03737 11.096 4.09597C11.0374 4.15458 10.9579 4.1875 10.875 4.1875C10.7921 4.1875 10.7126 4.15458 10.654 4.09597C10.5954 4.03737 10.5625 3.95788 10.5625 3.875C10.5625 3.79212 10.5954 3.71263 10.654 3.65403C10.7126 3.59542 10.7921 3.5625 10.875 3.5625C10.9579 3.5625 11.0374 3.59542 11.096 3.65403C11.1546 3.71263 11.1875 3.79212 11.1875 3.875Z"
+                      stroke="CurrentColor"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+                <span className="fw-5 font text-color-2 lh-16">Slider</span>
+              </a>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+
+  );
+}
